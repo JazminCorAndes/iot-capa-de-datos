0c118ea0c4c9ec77e5a5d3c0ed7cea2db06da3e6
Jazmin <jn.cordobap1@uniandes.edu.co>
2026-02-23 02:09:40 -0500

    Add daily average path to URLs

--- timescaleMonitoring/urls.py
+++ timescaleMonitoring/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("historical/data", download_csv_data, name="historical-data"),
+    path('dailyAvg/', daily_avg_by_city, name='daily-avg'),
 ]
